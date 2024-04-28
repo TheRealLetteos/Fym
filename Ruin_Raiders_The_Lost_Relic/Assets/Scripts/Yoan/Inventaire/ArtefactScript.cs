06a68baa0745c8ec150e87847aeb257c18d9e0ad
mqcfrank <88257038+mqcfrank@users.noreply.github.com>
2024-04-27 22:10:41 -0400

    Revert "Revert "[FYM-216] Faire le UI visuel de l'inventaire, [FYM-217] Rejoindre les ressource qui sont pickable dans l'inventaire, [FYYM-213] Faire script parent inventaire, [FYM-214] Faire les scripts des objets et [FYM-215] Créer les assets des objets""
    This reverts commit aafdf6b3cfabca63c2c0e63077159cd66bee8316.

---
+++ Ruin_Raiders_The_Lost_Relic/Assets/Scripts/Yoan/Inventaire/ArtefactScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fym
+{
+    public class ArtefactScript : MonoBehaviour, IPickable
+    {
+        [SerializeField] private InventoryScript _inventory;
+        public void AddPickable()
+        {
+            _inventory._artefact += 1;
+        }
+    }
+}
