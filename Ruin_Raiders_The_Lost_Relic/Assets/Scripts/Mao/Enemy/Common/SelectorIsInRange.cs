ce5caed889882db47df8fe46e956a16ca4e6c154
soaringalano <linmao78730@gmail.com>
2024-04-30 15:33:04 -0400

    modify slime animator controller (transitions among states) , add selector "Is In Range" which is suitable for attack or any object detection.

---
+++ Ruin_Raiders_The_Lost_Relic/Assets/Scripts/Mao/Enemy/Common/SelectorIsInRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MBT
+{
+
+    [MBTNode("AI/Selector Is In Range")]
+    [AddComponentMenu("")]
+    public class SelectorIsInRange : Selector
+    {
+        public TransformReference agentTransform;
+        public TransformReference targetTransform;
+        public float range = 10f;
+
+        public override NodeResult Execute()
+        {
+            if (Vector3.Distance(agentTransform.Value.position, targetTransform.Value.position) < range)
+            {
+                return NodeResult.success;
+            }
+            return NodeResult.failure;
+        }
+        
+    }
+}
