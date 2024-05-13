adba9935364bd047c8de1a1d85b86fe9ca9def0c
Letteos <Letteos@users.noreply.github.com>
2024-05-13 10:25:10 -0400

    [Fym-242] Utiliser les objets dans l'inventaire et [Fym-243] Avoir le compte des objets dans l'inventaire

--- Ruin_Raiders_The_Lost_Relic/Assets/Scripts/Yoan/Inventaire/LifePotionScript.cs
+++ Ruin_Raiders_The_Lost_Relic/Assets/Scripts/Yoan/Inventaire/LifePotionScript.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace fym
 {
     public class LifePotionScript : MonoBehaviour, IPickable, IUsableObject
     {
         [SerializeField] private InventoryScript _inventory;
         [SerializeField] private Player _player;
+        [SerializeField] private GameObject _potionButton;
         public void AddPickable()
         {
-            _inventory._lifePotion += 1;
+            if (_inventory._lifePotion < 1)
+            {
+                _inventory._lifePotion += 1;
+                _potionButton.SetActive(true);
+            }
         }
 
         public void Use()
@@ -18,6 +24,7 @@ namespace fym
             _player._life = 3;
             _player.UpdateHealth();
             _inventory._lifePotion -= 1;
+            _potionButton.SetActive(false);
         }
     }
 }
